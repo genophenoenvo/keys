61c63eb8f13cdc63ae78e452d2564db478c89eae
eshaan-joshi <joshie1@email.arizona.edu>
2020-07-02 00:09:37 +0000

    Segmentation

--- code/2020-06-23workingsegmentscript.R
+++ code/2020-06-23workingsegmentscript.R
@@ -6,16 +6,24 @@ library(lidR)
 #  install.packages("BiocManager")
 #BiocManager::install(version = "3.7")
 #BiocManager::install("EBImage")
-#las = readLAS("/home/rstudio/keys_work/keys/code/Example.las", select = "xyziar", filter = "-keep_first -drop_z_below_0")
+#las = readLAS("/home/rstudio/keys/code/Example.las", select = "xyziar", filter = "-keep_first -drop_z_below_0")
+
 #las = readLAS("/home/rstudio/keys_work/keys/code/NEON_D14_SRER_DP1_L090-1_2019091314_unclassified_point_cloud.laz")
-las = readLAS("/home/rstudio/NEON_SRER_DP1.30003.001_2019/2019/FullSite/D14/2019_SRER_3/L1/DiscreteLidar/ClassifiedPointCloud/NEON_D14_SRER_DP1_501000_3519000_classified_point_cloud_colorized.laz")
+#las = readLAS("/home/rstudio/NEON_SRER_DP1.30003.001_2019/2019/FullSite/D14/2019_SRER_3/L1/DiscreteLidar/ClassifiedPointCloud/NEON_D14_SRER_DP1_501000_3519000_classified_point_cloud_colorized.laz")
 #las = readLAS("/home/rstudio/NEON_SRER_DP1.30003.001_2019/2019/FullSite/D14/2019_SRER_3/L1/DiscreteLidar/ClassifiedPointCloud/NEON_D14_SRER_DP1_501000_3518000_classified_point_cloud_colorized.laz")
 plot(las)
 #finished
+ortho <- raster(x = "/home/rstudio/dav/iplant/commons/community_released/aes/srer/suas/2019/ecostate_mapping/products/Sept/15_g2/15_g2_ortho.tif")
+plot(ortho)
+chm = grid_canopy(ortho, res = 1, p2r(0.15))
+kernel = matrix(1,3,3)
+chm = raster::focal(ortho, w = kernel, fun = median, na.rm = TRUE)
 
-
+ttops = tree_detection(chm, lmf(5))
+raster::plot(ortho, col = height.colors(30))
+sp::plot(ttops, add = TRUE)
 #second chunk
-#las = lasground(las, csf())
+las = lasground(las, csf())
 #warning return number / number of returns not found. Data issue?
 plot(las, color = "Classification")
 #finished second chunk
@@ -68,10 +76,9 @@ plot(crowns, col = pastel.colors(100))
 #The issue this time is that I'm missing EMImage, but when I installed it before, it takes a about 10 minutes and there's no mention of it in the documentation.
 #finished eight chunk
 
-
 #ninth chunk
-contour = rasterToPolygons(crowns, dissolve = TRUE)
+#contour = rasterToPolygons(crowns, dissolve = TRUE)
 #loading required namespace:rgeos
-plot(chm, col = height.colors(50))
-plot(contour, add = T)
+#plot(chm, col = height.colors(50))
+#plot(contour, add = T)
 #finished
